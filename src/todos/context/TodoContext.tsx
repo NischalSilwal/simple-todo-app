e7fcf78d5044bc45283d64ef2650545a5828a35c
Nischal Silwal <nischalsilwalhtd@gmail.com>
2025-06-18 13:42:20 +0545

    feat: fix json parsing error in usetodos hook

--- src/todos/context/TodoContext.tsx
+++ src/todos/context/TodoContext.tsx
@@ -1,8 +1,7 @@
 'use client';
-import { createContext, useContext, ReactNode } from 'react';
+import { createContext, useContext, ReactNode, useState, useEffect } from 'react';
 import { Todo } from '@/todos/types/todo';
 import { useTodos } from '@/todos/hooks/useTodos';
-
 interface TodoContextType {
   todos: Todo[];
   addTodo: (title: string) => void;
@@ -13,8 +12,12 @@ interface TodoContextType {
 export const TodoContext = createContext<TodoContextType | null>(null);
 
 export function TodoProvider({ children }: { children: ReactNode }) {
+  const [isClient, setIsclient] = useState(false);
+  useEffect(() => setIsclient(true), []);
   const { todos, addTodo, toggleTodo, deleteTodo } = useTodos();
-
+  if (!isClient) {
+    return null;
+  }
   return (
     <TodoContext.Provider value={{ todos, addTodo, toggleTodo, deleteTodo }}>
       {children}
--- src/todos/hooks/useTodos.ts
+++ src/todos/hooks/useTodos.ts
@@ -4,18 +4,27 @@ import { Todo } from '@/todos/types/todo';
 import { v4 as uuidv4 } from 'uuid';
 
 export function useTodos() {
-  const [todos, setTodos] = useState<Todo[]>(() => {
-    if (typeof window !== 'undefined') {
-      const saved = localStorage.getItem('todos');
-      return saved ? JSON.parse(saved) : [];
-    }
-    return [];
-  });
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isHydrated, setIsHydrated] = useState(false);
 
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
+    const saved = localStorage.getItem('todos');
+  if (saved) {
+    try{
+      setTodos(JSON.parse(saved));
+    }catch(e) {
+    console.error('failed to parse todo from localStorage:', e);
+    setTodos([]); // Fallback to empty array if parsing fails
+    }
+  }
+    setIsHydrated(true); // flag to signal hydration complete
+  }, []);
 
+  useEffect(() => {
+    if(isHydrated) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  }, [todos, isHydrated]);
   const addTodo = (title: string) => {
     setTodos((prev) => [
       ...prev,
